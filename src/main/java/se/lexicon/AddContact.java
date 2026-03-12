372149b2fd618c5be9a91e4b3f4b472ae28c65e8
SMASobur <sikdar.se@gmail.com>
2026-03-12 13:49:45 +0100

    Modify the AddContact
    taking name, mobile, combining name and mobile to store.

--- src/main/java/se/lexicon/AddContact.java
+++ src/main/java/se/lexicon/AddContact.java
@@ -1,17 +1,39 @@
 package se.lexicon;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddContact {
     private Scanner scanner = new Scanner(System.in);
 
+    private static List<String> contactList = new ArrayList<>();
+
     public void run() {
         displayMenu();
 
-        int contact = GetUserChoice.getInt(scanner,"");
+       // int contact = GetUserChoice.getInt(scanner,"");
+
+        System.out.print("Enter Name: ");
+        String name = scanner.nextLine().trim();
+
+        System.out.print("Enter Mobile Number: ");
+        String mobile = scanner.nextLine().trim();
+
+        String combineContact = name + "|" +mobile;
+
+        if (contactList.contains(combineContact)){
+            System.out.println("Error: This contact already exists.");
+        } else if (name.isEmpty() ||mobile.isEmpty()){
+            System.out.println("Error: Name or Mobile can not be empty.");
+        } else {
+           contactList.add(combineContact);
+            System.out.println("Contact successfully added: " + combineContact);
+        }
+
     }
 
     private void displayMenu() {
-        System.out.println("\n**** Add new contact ****");
+        System.out.println("\n**** Add a new contact ****");
     }
 }
